78e8841df26a66a89bd9e72e1b7496d1e02494aa
MD ABDULLAH <mdabdullah161036@gmail.com>
2024-01-16 20:25:01 +0600

    load data with dynamic id

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ app.use(cors());
 app.get("/phones", (req, res) => {
   res.send(phones);
 });
+//dynamic with id
+app.get("/phones/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const phone = phones.find((phone) => phone.id === id) || {};
+  res.send(phone);
+  console.log("id is ", id);
+});
 
 app.listen(port, () => {
   console.log("server is running here", port);
